660b26430cd25547db7a72556b246ce24892785d
Austin Keener <keeneraustin@yahoo.com>
2019-08-04 20:20:44 -0400

    Updated regex as Yui's shadowjar naming changed.

--- src/main/java/net/dv8tion/discord/bootstrap/Bootstrap.java
+++ src/main/java/net/dv8tion/discord/bootstrap/Bootstrap.java
@@ -10,7 +10,7 @@ import java.util.regex.Pattern;
 public class Bootstrap
 {
     public static final String LATEST_BUILD_ROOT = "https://ci.dv8tion.net/job/Yui/lastCompletedBuild/artifact/build/libs/";
-    public static final String URL_REGEX = "\\<a href=\"Yui-withDependencies-[0-9]*\\.[0-9]*\\.[0-9]*_[0-9]*\\.jar\">(Yui-withDependencies-[0-9]*\\.[0-9]*\\.[0-9]*_[0-9]*\\.jar)\\<\\/a\\>";
+    public static final String URL_REGEX = "\\<a href=\"Yui-[0-9]*\\.[0-9]*\\.[0-9]*_[0-9]*-withDependencies\\.jar\">(Yui-[0-9]*\\.[0-9]*\\.[0-9]*_[0-9]*-withDependencies\\.jar)\\<\\/a\\>";
 
     public static final String VERSION = "1.1.0";
     public static final File BOT_JAR_FILE = new File("./Yui.jar");
@@ -185,6 +185,7 @@ public class Bootstrap
         String page = Downloader.webpage(LATEST_BUILD_ROOT);
         Pattern urlPattern = Pattern.compile(URL_REGEX);
         Matcher urlMatcher = urlPattern.matcher(page);
+        System.out.println(page);
         if (urlMatcher.find())
         {
             return LATEST_BUILD_ROOT + urlMatcher.group(1);
